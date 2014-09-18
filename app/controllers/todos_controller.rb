2c584e77be72e809ea3bcd8030745b099b2b9ae6
Dan Gillespie <danielg@umd.edu>
2014-09-17 22:33:42 -0400

    Revert "added create"
    This reverts commit 65e262735de5619c63d9a6b73d98fa982caf0d0c.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,14 +6,4 @@ class TodosController < ApplicationController
 	def new
 		@todo = Todo.new
 	end
-
-	def create
-		@todo = Todo.new(params[:todo].permit(:title, :done))
-
-		if @todo.save
-			redirect_to "/todos"
-		else
-			render :new
-		end
-	end
 end
